a0130a7058d29cd824837f95d6929a8bcc731870
imam0321 <imam.hossain0321@gmail.com>
2025-05-12 02:06:18 +0600

    Add HowItWorks component and integrate it into the Home page; update global styles with new accent color

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Categories from "@/components/Categories";
 import FeaturedSurprises from "@/components/FeaturedSurprises";
 import Hero from "@/components/Hero";
+import HowItWorks from "@/components/HowItWorks";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <Categories />
       <FeaturedSurprises />
+      <HowItWorks />
     </>
   );
 }
