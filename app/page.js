2e2672cb213aa349f63153df1da3ac4f6b32c299
imam0321 <imam.hossain0321@gmail.com>
2025-05-12 00:41:50 +0600

    Add Categories component and integrate it into the Home page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
+import Categories from "@/components/Categories";
 import Hero from "@/components/Hero";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
-      <Hero/>
+      <Hero />
+      <Categories />
     </>
   );
 }
