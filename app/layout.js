8b5eed4ffac74b8408207cea58d1310dcb0a1241
imam0321 <imam.hossain0321@gmail.com>
2025-05-12 02:26:48 +0600

    Implement code changes to enhance functionality and improve performance

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Special Surprise",
@@ -22,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
         <div className="min-h-screen flex flex-col">
           <Navbar />
